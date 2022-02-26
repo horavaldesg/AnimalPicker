c05878902c26291ac061223c5bbc95ca94f7d029
Horacio Valdes <horavaldesg@gmail.com>
2022-02-25 23:00:32 -0500

    User Input

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class AnimalPicker{
     public static void main(String[] args) {
-        
-        
+        Scanner kb = new Scanner(System.in);
+        System.out.println("What animal would you like to see? Dog, Cat");
+        String animal = kb.next().toLowerCase();
+       switch(animal){
+           case "dog":
+           System.out.println(animal +" art goes here");
+           break;
+           case "cat":
+           System.out.println(animal +" art goes here");
+           break;
+       }
     }
+
+   
 }
